7fc382aa3a07c10e7c14455d649a54cc14408ef7
Muhammad Sajid <sajiiidali@gmail.com>
2022-06-04 01:26:08 -0700

    Android JectPack Navigation Component Sample

---
+++ app/src/main/java/com/digital/finger/pk/jetpackNavigationSample/fragments/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.digital.finger.pk.jetpackNavigationSample.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.digital.finger.pk.jetpackNavigationSample.R
+
+class HomeFragment: Fragment(R.layout.homefragment) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
